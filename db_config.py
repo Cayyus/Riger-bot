5b09a1e56a57f289e5085ee3bb13ea394e50caf6
Caius <117181766+Cayyus@users.noreply.github.com>
2024-02-22 14:19:36 +0600

    added timeout logic

--- db_config.py
+++ db_config.py
@@ -2,7 +2,6 @@ import sqlite3
 
 db = sqlite3.connect('coins.db')
 
-#cursor obj
 cursor = db.cursor()
 
 class UserDB:
@@ -19,7 +18,7 @@ class UserDB:
             username TEXT NOT NULL UNIQUE,
             user_id INTEGER NOT NULL UNIQUE,
             coins INTEGER NOT NULL,
-            last_ran INTEGER NOT NULL
+            last_ran INTEGER NOT NULL,
         );
         """)
     
@@ -44,6 +43,25 @@ class UserDB:
         cursor.execute(f"SELECT * FROM users WHERE user_id = ?;", (self.uid,))
         row = cursor.fetchall()
         return row
+    
+    def get_coin_count_all(self):
+        cursor.execute("SELECT username, coins FROM users;")
+        users = cursor.fetchall()
+        sorted_users = sorted(users, key=lambda x: x[1], reverse=True)
+        result_dict = {user[0]: user[1] for user in sorted_users}
+        return result_dict    
+    
+    def get_coin_count(self):
+        cursor.execute(f"SELECT username, user_id, coins WHERE user_id = ?", (self.uid,))
+        values = cursor.fetchall()[0]
+        (username, user_id, coin_count,) = values
+        return username, user_id, coin_count
+    
+    def get_last_played(self):
+        cursor.execute('SELECT last_ran FROM users WHERE user_id = ?', (self.uid,))
+        timestamp = cursor.fetchall()[0]
+        (timestamp,) = timestamp
+        return timestamp
 
     def update_coin_count(self):
         cursor.execute("SELECT coins FROM users WHERE user_id = ?;", (self.uid,))
@@ -53,12 +71,10 @@ class UserDB:
         cursor.execute("UPDATE users SET coins = ? where user_id = ?", (new_coins, self.uid,))
         db.commit()
     
-    def update_timestamp(self):
-        cursor.execute("UPDATE users SET last_ran = ? where user_id = ?", (self.last_played, self.uid,))
+    def update_timestamp(self, timestamp):
+        cursor.execute("UPDATE users SET last_ran = ? where user_id = ?", (timestamp, self.uid,))
         db.commit()
     
     def update_username(self):
         cursor.execute("UPDATE users SET username = ? where user_id = ?", (self.username, self.uid,))
         db.commit()
-    
-
