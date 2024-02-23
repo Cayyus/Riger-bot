16c658e9fe2ae5ccac5dabcbeec1bf4fbd55ac78
Caius <117181766+Cayyus@users.noreply.github.com>
2024-02-23 10:35:17 +0600

    Update autorole.py

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -1,20 +1,20 @@
-import discord
 import asyncio
 from discord.ext import commands
-from discord import app_commands
+from discord import app_commands, Role, Interaction
+from discord.utils import get
 
 class AutoRoleCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name='role', description='Gives a specified role to a member or multiple members')
-    async def roles(self, interaction: discord.Interaction, role: discord.Role, members: str):
+    async def roles(self, interaction: Interaction, role: Role, members: str):
         await interaction.response.defer()
         await process_role_action(interaction, role, members, add=True)
         await interaction.followup.send('Done.')
 
     @app_commands.command(name='derole', description='Removes a specified role from a member or multiple members')
-    async def derole(self, interaction: discord.Interaction, role: discord.Role, members: str):
+    async def derole(self, interaction: Interaction, role: Role, members: str):
         await interaction.response.defer()
         await process_role_action(interaction, role, members, add=False)
         await interaction.followup.send('Done.')
@@ -24,7 +24,7 @@ async def process_role_action(interaction, role, members, add=True):
     guild = interaction.guild
 
     for member_id in member_ids:
-        server_member = discord.utils.get(guild.members, id=member_id)
+        server_member = get(guild.members, id=member_id)
 
         if server_member is not None:
             if add:
