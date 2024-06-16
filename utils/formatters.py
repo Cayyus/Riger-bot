d61881723240e5c72ffe1f7ec8479dcc0bff62b9
Caius <117181766+Cayyus@users.noreply.github.com>
2024-06-16 20:56:13 +0600

    Update formatters.py

--- utils/formatters.py
+++ utils/formatters.py
@@ -17,6 +17,34 @@ status_emotes = {
     'idle': 1162244916932521984
 }
 
+trivia_categories = [
+    {"name": 'Pick random categories', 'value': 'random'},
+    {'name': 'General Knowledge', 'value': '9'},
+    {'name': 'Books', 'value': '10'},
+    {'name': 'Film', 'value': '11'},
+    {'name': 'Music', 'value': '12'},
+    {'name': 'Musicals & Theatres', 'value': '13'},
+    {'name': 'Television', 'value': '14'},
+    {'name': 'Video Games', 'value': '15'},
+    {'name': 'Board Games', 'value': '16'},
+    {'name': 'Nature', 'value': '17'},
+    {'name': 'Computers', 'value': '18'},
+    {'name': 'Mathematics', 'value': '19'},
+    {'name': 'Mythology', 'value': '20'},
+    {'name': 'Sports', 'value': '21'},
+    {'name': 'Geography', 'value': '22'},
+    {'name': 'History', 'value': '23'},
+    {'name': 'Politics', 'value': '24'},
+    {'name': 'Art', 'value': '25'},
+    {'name': 'Celebrities', 'value': '26'},
+    {'name': 'Animals', 'value': '27'},
+    {'name': 'Vehicles', 'value': '28'},
+    {'name': 'Comics', 'value': '29'},
+    {'name': 'Gadgets', 'value': '30'},
+    {'name': 'Anime & Manga', 'value': '31'},
+    {'name': 'Cartoon & Animations', 'value': '32'}
+]
+
 statuses = ['100000 people', 'Youtube', 'way too many people', 'Twitch', 'Fortnite streams', 'MrBeast']
 messages = cycle(statuses)
 
